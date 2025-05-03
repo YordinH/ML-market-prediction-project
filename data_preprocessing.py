14ab789079c35d1363ba18760706537843a27984
Yordin Hasty <65691779+YordinH@users.noreply.github.com>
2025-05-02 20:23:06 -0400

    Update data_preprocessing.py
    Added symbols to be analyzed and coded download portion

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
+
+symbols = ["NQ=F", "ES=F", "YM=F"] 
+time_interval = "5m"
+rangeStart = "2024-12-01"
+rangeEnd = "2025-04-01"
+
+rawData = {
+    symbol: yf.download(symbol, interval="5m", start=rangeStart, end=rangeEnd) for symbol in symbols
+}
